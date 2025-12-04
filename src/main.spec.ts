9a64e1b3b3c51e7d4c4cce7a6b5025aeacfd5b0b
Guillermo Diaz <guillermodiaz@acidtango.com>
2025-12-04 11:47:37 +0000

    test: modified test in order to use drinkType

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,11 @@
 import { describe, expect, it } from "vitest"
 import { CustomerOrder, serveCoffe } from "./main.js"
+import { drinkType } from "./main.js"
 
 describe("Serve coffe test", () => {
   it("serve coffe and no sugar", () => {
     const customerOrder: CustomerOrder = {
-      drink: "Coffee",
+      drink: drinkType.Coffe,
       sugar: 0
     }
     const result = serveCoffe(customerOrder)
@@ -14,12 +15,23 @@ describe("Serve coffe test", () => {
 
   it("serve tea with 2 sugar", () => {
     const customerOrder: CustomerOrder = {
-      drink: "Tea",
+      drink: drinkType.Tea,
       sugar: 2
     }
 
     const result = serveCoffe(customerOrder)
     expect(result).toBe("T:2:0")
+
+  })
+
+  it("serve chocolate with 2 sugar", () => {
+    const customerOrder: CustomerOrder = {
+      drink: drinkType.Chocolate,
+      sugar: 1
+    }
+
+    const result = serveCoffe(customerOrder)
+    expect(result).toBe("H:1:0")
 
   })
 
