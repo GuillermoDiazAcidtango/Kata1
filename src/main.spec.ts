9a162a607ab5571b620fbbb59dcab897a0ad989c
Guillermo Diaz <guillermodiaz@acidtango.com>
2025-12-04 11:26:32 +0000

    test: add test for serve coffe use case

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,13 @@
-import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { describe, expect, it } from "vitest"
+import { CustomerOrder, serveCoffe } from "./main.js"
 
-describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
-
-    expect(result).toBe(true)
+describe("Serve coffe test", () => {
+  it("serve coffe and no sugar", () => {
+    const customerOrder: CustomerOrder = {
+      drink: "coffee",
+      sugar: 0
+    }
+    const result = serveCoffe(customerOrder)
+    expect(result).toBe("C::")
   })
 })
