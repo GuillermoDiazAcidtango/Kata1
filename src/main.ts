fb61b97791ed1d353a9dad58ab4b9cded411b449
Guillermo Diaz <guillermodiaz@acidtango.com>
2025-12-04 11:26:43 +0000

    feat: created serveCoffe function

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
-export function myFunction() {
-  return true
+export function serveCoffe(order: CustomerOrder): string {
+  let serveMessage = ""
+
+  serveMessage += drinkType
+
+  return ""
 }
+
+export type CustomerOrder = {
+  drink: string,
+  sugar: number
+}
+
+const drinkType = {
+  C: 'Coffee',
+  T: 'Tea',
+  H: 'Chocolate'
+} as const
+
+type DrinkType = (typeof drinkType)[keyof typeof drinkType]
